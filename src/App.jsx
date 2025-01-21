b432aecc854b58b5f2a9e24e83b876583e405be2
Dima <dmitryberesten@gmail.com>
2025-01-21 21:22:18 +0200

    react-template

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Example } from './components/example/Example';
+
+export const App = () => {
+  return (
+    <div>
+      <Example />
+    </div>
+  );
+};
---
+++ src/components/example/Example.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Example = () => {
+  return (
+      <h1>Welcome to My React App</h1>
+  );
+};
+
+export default Example;
+
