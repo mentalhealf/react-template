2757499e0763b1dd1af4a8666ec3172bf067f234
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2025-02-19 10:46:37 +0000

    t

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Product from './components/Product';
-
+import kfc from './images/kfc.png';
+import zinger from './images/zinger.jpg';
 export const App = () => {
   return (
     <div>
@@ -14,6 +15,16 @@ export const App = () => {
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         name="Fries and Burger"
         price={14.29}
+      />
+      <Product
+        imgUrl={kfc}
+        name="9 piece of Chiken and Drumsticks"
+        price={14.99}
+      />
+      <Product
+        imgUrl={zinger}
+        name="zinger the original recipe"
+        price={6.99}
       />
     </div>
   );
