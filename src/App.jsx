a0ebf708682a6c4d29c638e5afb0cb2a4c7da000
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2025-07-06 11:47:48 +0100

    as

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,20 @@ import Drinks from 'components/Drinks';
 import chocolate_icecream from './images/chocolate_icecream.jpg';
 import oreo_milkshake from './images/cholate_oreo_milkshake.jpg';
 import Donut from './images/Donuts.jpg';
-import Orange_juice from './images/drink1.jpg'
-import Mahito from './images/drink2.jpg'
-import Pina_colada from './images/drink3.jpg'
+import Orange_juice from './images/drink1.jpg';
+import Mahito from './images/drink2.jpg';
+import Pina_colada from './images/drink3.jpg';
+import Peperoni from './images/Peperoni.png';
+import Vegan_pizza from './images/Vegan_pizza.jpg';
+import premium_margarita from './images/Premium_margarita.jpg';
+import Premium_bacon from './images/Premium_bacon.png';
 
 export const App = () => {
   return (
     <div>
       <Header></Header>
       <div className="container">
-        <h1 className="product_best">Best selling products</h1>
+        <h1 className="product_best">Our products</h1>
         <div className="product_list">
           <Product
             imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
@@ -45,6 +49,26 @@ export const App = () => {
             imgUrl={zinger}
             name="zinger the original recipe"
             price={6.99}
+          />
+          <Product
+            imgUrl={Peperoni}
+            name="Premium Pizza Peperoni"
+            price={10.99}
+          />
+          <Product
+            imgUrl={Premium_bacon}
+            name="Premium Bacon Pizza"
+            price={10.99}
+          />
+          <Product
+            imgUrl={premium_margarita}
+            name="Premium Margarita Pizza"
+            price={9.99}
+          />
+          <Product
+            imgUrl={Vegan_pizza}
+            name="Premium Vegan Pizza"
+            price={10.99}
           />
         </div>
       </div>
@@ -86,19 +110,26 @@ export const App = () => {
         </div>
       </div>
 
-              <h1 className="drinks_h1">Our Drinks</h1>
-        <div className="drinks_list">
-          <Drinks
-            imgUrl={Orange_juice}
-            name="Fresh Orange juice"
-            price={'1,50'}
-          />
+      <h1 className="drinks_h1">Our Drinks</h1>
+      <div className="drinks_list">
+        <Drinks
+          imgUrl={Orange_juice}
+          name="Fresh Orange juice"
+          price={'1,50'}
+        />
 
-          <Drinks imgUrl={Mahito} name="Mahito (non-alchoholic & alcoholic)" price={'3,50'} />
+        <Drinks
+          imgUrl={Mahito}
+          name="Mahito (non-alchoholic & alcoholic)"
+          price={'3,50'}
+        />
 
-          <Drinks imgUrl={Pina_colada} name="Pina Colada (non-alchoholic & alcoholic)" price={'5'} />
-        </div>
-
+        <Drinks
+          imgUrl={Pina_colada}
+          name="Pina Colada (non-alchoholic & alcoholic)"
+          price={'5'}
+        />
+      </div>
 
       <Footer></Footer>
     </div>
