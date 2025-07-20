339dcf9824647fb45fca0d24b9c582655bd0e916
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2025-07-20 12:36:56 +0100

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import logo from 'images/A-logo-for-a-fast-food-brand.png';
+import './Footer.css';
+
+const Footer = ({
+    // imglogo = {logo}
+
+}) => (
+    <footer className='footer'>
+      <img className='footer_logo' src={logo} width="100" />
+      <p className='footer_p'>This Website was made by <a className='footer_a' href="https://discord.com/users/1172955093189009448" target="_blank">Alex Lysov /// kovese</a> in 2025 <br/> All copy rights reserved© </p>
+      </footer>
+  );
 
 
-const Footer = ({}) => {
-    <div>
-    <footer>
-        <p>This site was made by <a href="https://t.me/christianfrogy">Alex Lysov©</a></p>
-    </footer>
-    </div>
-    
-}
 
-export default Footer;
+
+  export default Footer;
