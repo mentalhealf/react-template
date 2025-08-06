79a0f2cc28a7c9ed8d7fac21d39af42ada659b84
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2025-08-06 13:53:28 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,36 @@ import React from 'react';
 import logo from 'images/A-logo-for-a-fast-food-brand.png';
 import './Header.css';
 
-const Header = ({
+const Header = (
+  {
     // imglogo = {logo}
+  }
+) => (
+  <header className="header">
+    <img className="header_logo" src={logo} width="100" />
+    <ul className="header_list">
+      <li className="header_item">
+        <a className="header_link" href="#">
+          Home 🏠
+        </a>
+      </li>
+      <li className="header_item">
+        <a className="header_link" href="#">
+          About us 👯
+        </a>
+      </li>
+      <li className="header_item">
+        <a className="header_link" href="#">
+          Delivery ✉️
+        </a>
+      </li>
+      <li className="header_item">
+        <a className="header_link" href="#">
+          Contacts 📞
+        </a>
+      </li>
+    </ul>
+  </header>
+);
 
-}) => (
-    <header className='header'>
-      <img className='header_logo' src={logo} width="100" />
-      <ul className='header_list'>
-        <li className='header_item'><a className='header_link' href="#">Home</a></li>
-        <li className='header_item'><a className='header_link' href="#">About us</a></li>
-        <li className='header_item'><a className='header_link' href="#">Delivery</a></li>
-        <li className='header_item'><a className='header_link' href="#">Contacts</a></li>
-      </ul>
-      </header>
-  );
-
-
-
-
-  export default Header;
+export default Header;
